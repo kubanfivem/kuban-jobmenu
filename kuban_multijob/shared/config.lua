07e17a7ffb1873356aec590b56706f7fbfaab18d
kubanscripts <130844381+kubanfivem@users.noreply.github.com>
2023-12-27 14:20:27 +1000

    Add files via upload

---
+++ kuban_multijob/shared/config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+Config.MaxJobs = 5
+
+Config.JobIcons = {
+	['police'] = 'fa-solid fa-shield',
+	['ambulance'] = 'fa-solid fa-user-doctor',
+	['electrician'] = 'fa-solid fa-bolt',
+	['diving'] = 'fa-solid fa-truck-fish',
+	['builder'] = 'fa-solid fa-helmet-safety',
+	['judge'] = 'fa-solid fa-gavel',
+	['realestate'] = 'fa-solid fa-house',
+	['cardealer'] = 'fa-solid fa-car',
+	['garbage'] = 'fa-solid fa-trash',
+	['casino'] = 'fa-solid fa-dice',
+	['mechanic'] = 'fa-solid fa-wrench',
+	['tow'] = 'fa-solid fa-truck-tow',
+	['taxi'] = 'fa-solid fa-taxi',
+	['vineyard'] = 'fa-solid fa-wine-bottle',
+	['hotdog'] = 'fa-solid fa-hotdog',
+}
